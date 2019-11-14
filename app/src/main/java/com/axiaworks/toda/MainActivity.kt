0954b4083b5c364faf1130da2b76a4961656a224
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-15 02:06:48 +0900

    トップメニュー画面の作成

--- app/src/main/java/com/axiaworks/toda/MainActivity.kt
+++ app/src/main/java/com/axiaworks/toda/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.axiaworks.toda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_MESSAGE = "com.axiaworks.toda.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragment_button.setOnClickListener {
+//            val intent = Intent(this, FragmentsActivity::class.java).apply {
+//                putExtra(EXTRA_MESSAGE)
+//            }
+            val intent = Intent(this, FragmentsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
