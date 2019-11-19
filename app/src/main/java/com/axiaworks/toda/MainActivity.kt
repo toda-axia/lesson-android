e1a4542a7bcdb2f11a6de98c24944b7e21f5de17
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-19 14:04:58 +0900

    Activityの遷移処理を簡潔に記述するように変更

--- app/src/main/java/com/axiaworks/toda/MainActivity.kt
+++ app/src/main/java/com/axiaworks/toda/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.axiaworks.toda
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.axiaworks.toda.feature.viewpager.ViewPager2Activity
@@ -9,14 +7,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private fun showRecyclerView(context: Context) = context.startActivity(ViewPager2Activity.callingIntent(context))
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fragment_button.setOnClickListener {
-            showRecyclerView(this)
+            startActivity(ViewPager2Activity.callingIntent(this))
         }
     }
 }
