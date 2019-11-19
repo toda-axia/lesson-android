4e2178f2b62728b8a61de6434ce39d73052f0101
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-19 20:22:24 +0900

    Glide課題アプリの作成

--- app/src/main/java/com/axiaworks/toda/feature/glide/GlideActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/glide/GlideActivity.kt
@@ -5,8 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.axiaworks.toda.R
-import com.axiaworks.toda.feature.viewpager.ViewPager2Activity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.activity_glide.*
 
 class GlideActivity : AppCompatActivity() {
@@ -20,18 +21,44 @@ class GlideActivity : AppCompatActivity() {
 
         Glide.with(imageView1)
             .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .error(R.mipmap.ic_launcher)
             .into(imageView1)
 
         Glide.with(imageView2)
             .load("https://contents.oricon.co.jp/special/img/52000/52822/detail/img480/1554804245661.jpg")
+            .error(R.mipmap.ic_launcher)
             .into(imageView2)
-//
-//        Glide.with(imageView3)
-//            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg\thttps://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
-//            .into(imageView3)
-//
-//        Glide.with(imageView4)
-//            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg\thttps://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
-//            .into(imageView4)
+
+        Glide.with(imageView3)
+            .load("https://s.aolcdn.com/hss/storage/midas/ab3657e22f040a9f99ce0e3f5de3231b/203475401/honda.jpg")
+            .error(R.mipmap.ic_launcher)
+            .into(imageView3)
+
+        Glide.with(imageView4)
+            .load("")
+            .error(R.mipmap.ic_launcher)
+            .into(imageView4)
+
+        // 角丸加工
+        Glide.with(imageView5)
+            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(30)))
+            .error(R.mipmap.ic_launcher)
+            .into(imageView5)
+
+        Glide.with(imageView6)
+            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .error(R.mipmap.ic_launcher)
+            .into(imageView6)
+
+        Glide.with(imageView7)
+            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .error(R.mipmap.ic_launcher)
+            .into(imageView7)
+
+        Glide.with(imageView8)
+            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .error(R.mipmap.ic_launcher)
+            .into(imageView8)
     }
 }
