9e6482ca724a946590f2f8ae5346801e669e3b5c
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-20 21:22:08 +0900

    Firebase課題アプリの実装

--- app/src/main/java/com/axiaworks/toda/feature/firebase/FirebaseAnalyticsActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/firebase/FirebaseAnalyticsActivity.kt
@@ -2,17 +2,69 @@ package com.axiaworks.toda.feature.firebase
 
 import android.content.Context
 import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.axiaworks.toda.R
+import kotlinx.android.synthetic.main.activity_firebase_analytics.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FirebaseAnalyticsActivity : AppCompatActivity() {
+
+    private val logDataSet = arrayListOf<String>()
+    val log_adapter = LogRecyclerViewAdapter(logDataSet)
+
     companion object {
+        val TAG: String = "Firebase"
         fun callingIntent(context: Context) = Intent(context, FirebaseAnalyticsActivity::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_analytics)
+
+        log_button1.setOnClickListener {
+            addLogData(1)
+            initRecyclerView()
+        }
+
+        log_button2.setOnClickListener {
+            addLogData(2)
+            initRecyclerView()
+        }
+
+        log_button3.setOnClickListener {
+            addLogData(3)
+            initRecyclerView()
+            log_adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun getTimeStamp(): String {
+        val date = Date()
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return format.format(date)
+    }
+
+    fun addLogData(button_number: Int) {
+        val logDateTime: String = getTimeStamp()
+        val logData: String = "$logDateTime: click ボタン$button_number"
+        Log.d(TAG, "$logData")
+        logDataSet.add(logData)
+        Log.d(TAG, "$logDataSet")
+        if (logDataSet.size > 100) {
+            logDataSet.removeAt(0)
+        }
+    }
+
+    private fun initRecyclerView() {
+        log_view.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@FirebaseAnalyticsActivity)
+            adapter = LogRecyclerViewAdapter(logDataSet)
+        }
     }
 }
---
+++ app/src/main/java/com/axiaworks/toda/feature/firebase/LogRecyclerViewAdapter.kt
@@ -0,0 +1,44 @@
+package com.axiaworks.toda.feature.firebase
+
+import androidx.recyclerview.widget.RecyclerView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.axiaworks.toda.R
+
+
+class LogRecyclerViewAdapter(myDataSet: ArrayList<String>) : RecyclerView.Adapter<LogRecyclerViewAdapter.ViewHolder>() {
+
+    private var dataSet = arrayListOf<String>()
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var mTextView: TextView
+
+        init {
+            mTextView = v.findViewById(R.id.log_text) as TextView
+        }
+    }
+
+    init {
+        dataSet = myDataSet
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_log_info, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mTextView.text = dataSet[position]
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+
+}
