bb592fcc600c5e44cfbb2024b792ebaf90b1ac26
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-20 12:08:24 +0900

    レビュー後の修正

--- app/src/main/java/com/axiaworks/toda/feature/glide/GlideActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/glide/GlideActivity.kt
@@ -15,6 +15,9 @@ import kotlinx.android.synthetic.main.activity_glide.*
 
 class GlideActivity : AppCompatActivity() {
     companion object {
+        const val imageURL = "https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg"
+        const val imageURL2 = "https://contents.oricon.co.jp/special/img/52000/52822/detail/img480/1554804245661.jpg"
+        const val imageURL3 = "https://s.aolcdn.com/hss/storage/midas/ab3657e22f040a9f99ce0e3f5de3231b/203475401/honda.jpg"
         fun callingIntent(context: Context) = Intent(context, GlideActivity::class.java)
     }
 
@@ -23,17 +26,17 @@ class GlideActivity : AppCompatActivity() {
         setContentView(R.layout.activity_glide)
 
         Glide.with(imageView1)
-            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .load(imageURL)
             .error(R.mipmap.ic_launcher)
             .into(imageView1)
 
         Glide.with(imageView2)
-            .load("https://contents.oricon.co.jp/special/img/52000/52822/detail/img480/1554804245661.jpg")
+            .load(imageURL2)
             .error(R.mipmap.ic_launcher)
             .into(imageView2)
 
         Glide.with(imageView3)
-            .load("https://s.aolcdn.com/hss/storage/midas/ab3657e22f040a9f99ce0e3f5de3231b/203475401/honda.jpg")
+            .load(imageURL3)
             .error(R.mipmap.ic_launcher)
             .into(imageView3)
 
@@ -44,28 +47,28 @@ class GlideActivity : AppCompatActivity() {
 
         // 角丸加工
         Glide.with(imageView5)
-            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .load(imageURL)
             .apply(RequestOptions.bitmapTransform(RoundedCorners(30)))
             .error(R.mipmap.ic_launcher)
             .into(imageView5)
 
         // ぼかし加工
         Glide.with(imageView6)
-            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .load(imageURL)
             .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
             .error(R.mipmap.ic_launcher)
             .into(imageView6)
 
         // トゥーン加工
         Glide.with(imageView7)
-            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .load(imageURL)
             .apply(RequestOptions.bitmapTransform(ToonFilterTransformation(10.0f, 5.0f)))
             .error(R.mipmap.ic_launcher)
             .into(imageView7)
 
         // コントラスト加工
         Glide.with(imageView8)
-            .load("https://cdn.narinari.com/site_img/photox/201909/23/20190922038.jpg")
+            .load(imageURL)
             .error(R.mipmap.ic_launcher)
                 .apply(RequestOptions.bitmapTransform(ContrastFilterTransformation(1.8f)))
             .into(imageView8)
