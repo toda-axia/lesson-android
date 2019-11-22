5c74145634b2aa83d68aefe00afddd9cea2d6ec4
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-22 15:52:50 +0900

    Warning対応

--- app/src/main/java/com/axiaworks/toda/feature/firebase/FirebaseAnalyticsActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/firebase/FirebaseAnalyticsActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.axiaworks.toda.R
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_firebase_analytics.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,6 +17,8 @@ class FirebaseAnalyticsActivity : AppCompatActivity() {
 
     val logDataSet = arrayListOf<String>()
     val adapter = LogRecyclerViewAdapter(logDataSet)
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     companion object {
         val TAG: String = "Firebase"
@@ -26,17 +29,22 @@ class FirebaseAnalyticsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_analytics)
 
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
         log_button1.setOnClickListener {
+            logButtonAnalytics(1)
             addLogData(1)
             initRecyclerView()
         }
 
         log_button2.setOnClickListener {
+            logButtonAnalytics(2)
             addLogData(2)
             initRecyclerView()
         }
 
         log_button3.setOnClickListener {
+            logButtonAnalytics(3)
             addLogData(3)
             initRecyclerView()
         }
@@ -69,5 +77,12 @@ class FirebaseAnalyticsActivity : AppCompatActivity() {
             var scrollPosition = logDataSet.size - 1
             log_view.scrollToPosition(scrollPosition)
         }
+    }
+
+    private fun logButtonAnalytics(button_number: Int) {
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "contentType")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "firebase_button$button_number")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
