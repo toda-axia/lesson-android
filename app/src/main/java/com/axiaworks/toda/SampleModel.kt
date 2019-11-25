ef70dfc8b3a9798fc5a5d76bce17f0d96003a20e
t-nakata <nakata0615@gmail.com>
2019-11-25 17:12:00 +0900

    データバインディングのサンプル

---
+++ app/src/main/java/com/axiaworks/toda/SampleModel.kt
@@ -0,0 +1,6 @@
+package com.axiaworks.toda
+
+data class SampleModel(
+    val button1_label: String,
+    val button2_label: String
+)
--- app/src/main/java/com/axiaworks/toda/feature/dialogfragment/DialogFramentActivityFragment.kt
+++ app/src/main/java/com/axiaworks/toda/feature/dialogfragment/DialogFramentActivityFragment.kt
@@ -1,15 +1,14 @@
 package com.axiaworks.toda.feature.dialogfragment
 
-import android.content.DialogInterface
-import android.graphics.Color
-import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.axiaworks.toda.R
-import kotlinx.android.synthetic.main.dialog_content.view.*
+import com.axiaworks.toda.SampleModel
+import com.axiaworks.toda.databinding.FragmentDialogFramentBinding
 import kotlinx.android.synthetic.main.fragment_dialog_frament.*
 
 /**
@@ -17,23 +16,42 @@ import kotlinx.android.synthetic.main.fragment_dialog_frament.*
  */
 class DialogFramentActivityFragment : Fragment() {
 
+    private lateinit var binding: FragmentDialogFramentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = LayoutInflater.from(context).inflate(R.layout.dialog_content, container, false)
-        v.dialog_text_view.text = "Fragment"
-        v.dialog_text_view.setTextColor(Color.BLACK)
-        return v
+        binding = DataBindingUtil.inflate<FragmentDialogFramentBinding>(
+            LayoutInflater.from(context),
+            R.layout.fragment_dialog_frament,
+            container,
+            false
+        ).apply {
+            model = SampleModel("ボタン１", "ボタン２")
+        }
+
+
+        val v =
+            LayoutInflater.from(context).inflate(R.layout.fragment_dialog_frament, container, false)
+//        v.dialog_text_view.text = "Fragment"
+//        v.dialog_text_view.setTextColor(Color.BLACK)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.dialogFragmentButton1?.setOnClickListener {
+
+        }
+
         dialog_fragment_button1.setOnClickListener {
             val newFragment = SimpleDialogFragment()
-            newFragment.show(supportFragmentManager, "")
+
+            newFragment.show(requireFragmentManager(), "")
         }
     }
 }
