cec7b95e6e847a4259905da69148a250a8032ac8
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-28 03:03:51 +0900

    レイアウトの作成

---
+++ app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
@@ -0,0 +1,12 @@
+package com.axiaworks.toda.feature.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetrofitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit)
+    }
+}
