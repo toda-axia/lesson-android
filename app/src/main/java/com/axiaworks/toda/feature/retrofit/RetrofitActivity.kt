efe6d54d0885fced5aa34f01bf2933c71f22333e
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-28 03:09:23 +0900

    トップメニューからの画面遷移処理の実装

--- app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
@@ -1,10 +1,15 @@
 package com.axiaworks.toda.feature.retrofit
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.axiaworks.toda.R
 
 class RetrofitActivity : AppCompatActivity() {
+    companion object {
+        fun callingIntent(context: Context) = Intent(context, RetrofitActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
