32629bf32dfed74dd09d93f80fa4cb75748c223c
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-27 17:59:10 +0900

    SimpleDialogの修正

--- app/src/main/java/com/axiaworks/toda/feature/dialog_fragment/SimpleDialog.kt
+++ app/src/main/java/com/axiaworks/toda/feature/dialog_fragment/SimpleDialog.kt
@@ -2,15 +2,27 @@ package com.axiaworks.toda.feature.dialog_fragment
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.axiaworks.toda.R
+import kotlinx.android.synthetic.main.simple_dialog.view.*
 
 class SimpleDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         this.isCancelable = false
+
+        val simpleDialogView = activity!!.layoutInflater.inflate(R.layout.simple_dialog, null)
+        val okButton = simpleDialogView.findViewById(R.id.dialogfragment_dialog1_ok_button) as Button
+        okButton.setOnClickListener {
+            dismissAllowingStateLoss()
+            Log.d("TAG", "メソッド呼べてる")
+        }
+
         return AlertDialog.Builder(requireContext())
             .apply {
 //                setTitle(R.string.dialog_fragment1)
@@ -20,5 +32,16 @@ class SimpleDialog : DialogFragment() {
 //
                 setView(LayoutInflater.from(getActivity()).inflate(R.layout.simple_dialog, null))
             }.create()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val simpleDialogView = activity!!.layoutInflater.inflate(R.layout.simple_dialog, null)
+        val ok_button = simpleDialogView.findViewById(R.id.dialogfragment_dialog1_ok_button) as Button
+        ok_button.setOnClickListener {
+            dismissAllowingStateLoss()
+            Log.d("TAG", "メソッド呼べてる")
+        }
     }
 }
