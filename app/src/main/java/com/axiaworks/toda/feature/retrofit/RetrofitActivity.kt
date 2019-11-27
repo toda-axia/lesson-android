cea28cc36c4c13ac734f63ca904c3f3508432ce0
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-28 03:06:00 +0900

    マニフェストファイルの微修正

--- app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
@@ -2,6 +2,7 @@ package com.axiaworks.toda.feature.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.axiaworks.toda.R
 
 class RetrofitActivity : AppCompatActivity() {
 
