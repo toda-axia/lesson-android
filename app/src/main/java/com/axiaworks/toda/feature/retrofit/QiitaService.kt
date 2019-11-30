5d7fee4cdc54206ddbe4ca2d974a51e0a8d1b010
toda-axia <hiroyuki.toda@axia-works.com>
2019-11-30 13:55:13 +0900

    Qiitaの記事のタイトルを10件表示するように変更

--- app/src/main/java/com/axiaworks/toda/feature/retrofit/QiitaService.kt
+++ app/src/main/java/com/axiaworks/toda/feature/retrofit/QiitaService.kt
@@ -1,11 +1,10 @@
 package com.axiaworks.toda.feature.retrofit
 
 import io.reactivex.Observable
-import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface QiitaService {
-    @GET("/api/v2/tags/{tag}/items?page=1&per_page=3")
-    fun getItemsByTag(@Path("tag") tag: String): Observable<ResponseBody>
+    @GET("/api/v2/tags/{tag}/items?page=1&per_page=10")
+    fun getItemsByTag(@Path("tag") tag: String): Observable<List<QiitaInfo>>
 }
--- app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/retrofit/RetrofitActivity.kt
@@ -4,16 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.axiaworks.toda.R
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.plugins.RxJavaPlugins.onError
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_retrofit.*
-import okhttp3.ResponseBody
 
 class RetrofitActivity : AppCompatActivity() {
-    private lateinit var observable: Observable<ResponseBody>
+    private lateinit var observable: Observable<List<QiitaInfo>>
     private lateinit var disposable: Disposable
 
     companion object {
@@ -24,26 +25,31 @@ class RetrofitActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit)
 
-        get_article_button.setOnClickListener {
-            getQiitaArticle()
-        }
-    }
-
-    private fun getQiitaArticle() {
         val service = QiitaClient().getClient().create(QiitaService::class.java)
+
         observable = service.getItemsByTag("Android")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .retry(3)
-        disposable = observable.subscribe {
-                responseBody: ResponseBody? ->
-                responseBody?.let {
-                    val body = it.string()
-                    if (body.isNotEmpty()) {
-                        result_article_text.text = body
-                    }
-                }
-            }
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        disposable = observable.subscribe(
+            { list -> doSomethingToList(list) },
+            { t -> onError(t) }
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        disposable.dispose()
+    }
+
+    private fun doSomethingToList(list: List<QiitaInfo>) {
+        qiita_title_view.adapter = QiitaInfoAdapter(this, list)
+        qiita_title_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
