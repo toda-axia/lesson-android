d60c128221b1ffe839214474a6b9f600aa10bd94
toda-axia <hiroyuki.toda@axia-works.com>
2019-12-01 18:37:15 +0900

    DatePickerで設定した値をテキストフィールドに入力

--- app/src/main/java/com/axiaworks/toda/feature/dialog_fragment2/DatePickerDialog.kt
+++ app/src/main/java/com/axiaworks/toda/feature/dialog_fragment2/DatePickerDialog.kt
@@ -2,15 +2,23 @@ package com.axiaworks.toda.feature.dialog_fragment2
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import java.text.SimpleDateFormat
 import java.util.*
 
 class DatePickerDialog: DialogFragment(), DatePickerDialog.OnDateSetListener {
 
+    private var listener: OnDateSetListener? = null
+
     companion object {
         const val TAG = "DatePickerDialog"
+    }
+
+    interface OnDateSetListener {
+        fun onDateSelected(dateString: String)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -22,7 +30,25 @@ class DatePickerDialog: DialogFragment(), DatePickerDialog.OnDateSetListener {
         return DatePickerDialog(requireContext(), this, year, month, day)
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as? OnDateSetListener
+    }
+
+    override fun onDetach() {
+        listener = null
+        super.onDetach()
+    }
+
+    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, day: Int) {
+        val dateString = getDateString(year, month, day)
+        listener?.onDateSelected(dateString)
+    }
+
+    private fun getDateString(year: Int, month: Int, day: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.JAPAN)
+        return dateFormat.format(calendar.time)
     }
 }
--- app/src/main/java/com/axiaworks/toda/feature/dialog_fragment2/DialogFragment2Activity.kt
+++ app/src/main/java/com/axiaworks/toda/feature/dialog_fragment2/DialogFragment2Activity.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.axiaworks.toda.R
+import kotlinx.android.synthetic.main.fragment_dialog2.*
 
-class DialogFragment2Activity : AppCompatActivity() {
+class DialogFragment2Activity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     private var dialogFragment2Fragment: DialogFragment2Fragment? = null
 
     companion object {
@@ -18,5 +20,11 @@ class DialogFragment2Activity : AppCompatActivity() {
         setContentView(R.layout.activity_dialog_fragment2)
 
         dialogFragment2Fragment = supportFragmentManager.findFragmentById(R.id.dialogfragment_container) as DialogFragment2Fragment
+    }
+
+    override fun onDateSelected(dateString: String) {
+        val inputDateText = findViewById<EditText>(R.id.inputDateText)
+        inputDateText.setText(dateString)
+        //inputDateText.text = dateString
     }
 }
