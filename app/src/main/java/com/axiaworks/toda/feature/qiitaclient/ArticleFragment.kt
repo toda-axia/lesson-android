5e3bddafd623847eea6ccfcbbb6f24fb6cf026bc
toda-axia <hiroyuki.toda@axia-works.com>
2019-12-11 02:10:47 +0900

    タグごとの表示するメソッドを作成

--- app/src/main/java/com/axiaworks/toda/feature/qiitaclient/ArticleFragment.kt
+++ app/src/main/java/com/axiaworks/toda/feature/qiitaclient/ArticleFragment.kt
@@ -1,18 +1,13 @@
 package com.axiaworks.toda.feature.qiitaclient
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.axiaworks.toda.R
-import kotlinx.android.synthetic.main.activity_qiita_client.*
 import kotlinx.android.synthetic.main.fragment_article.*
-import kotlinx.android.synthetic.main.list_item_qiita_info.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class ArticleFragment: Fragment() {
@@ -63,28 +58,26 @@ class ArticleFragment: Fragment() {
 
         swipe_to_refresh_qiita_client.setOnRefreshListener{
             swipe_to_refresh_qiita_client.isRefreshing = false
-            if (tagText == "Android") {
-                qiitaClientViewModel.getAndroidArticle()
-            }
-            if (tagText == "Firebase") {
-                qiitaClientViewModel.getFirebaseArticle()
-            }
-            if (tagText == "Flutter") {
-                qiitaClientViewModel.getFlutterArticle()
-            }
+            displayContentByTag(tagText)
         }
     }
 
     override fun onResume() {
         super.onResume()
-        if (tagText == "Android") {
-            qiitaClientViewModel.getAndroidArticle()
-        }
-        if (tagText == "Firebase") {
-            qiitaClientViewModel.getFirebaseArticle()
-        }
-        if (tagText == "Flutter") {
-            qiitaClientViewModel.getFlutterArticle()
+        displayContentByTag(tagText)
+    }
+
+    fun displayContentByTag(tag: String) {
+        when (tagText) {
+            "Android" -> {
+                qiitaClientViewModel.getAndroidArticle()
+            }
+            "Firebase" -> {
+                qiitaClientViewModel.getFirebaseArticle()
+            }
+            "Flutter" -> {
+                qiitaClientViewModel.getFlutterArticle()
+            }
         }
     }
 }
